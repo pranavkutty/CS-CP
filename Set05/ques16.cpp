cf2ad64417b6ae810503b7bb9693f20f6c9a2074
Pranav Kutty <pranavkutty13@gmail.com>
2021-07-13 23:26:28 +0530

    linked list

---
+++ Set05/ques16.cpp
@@ -0,0 +1,8 @@
+// delete node in linked list
+// https://leetcode.com/problems/delete-node-in-a-linked-list/
+void deleteNode(ListNode* node) {
+    ListNode* temp = node->next;
+    node->val = temp->val;
+    node->next = temp->next;
+    delete temp;
+}
